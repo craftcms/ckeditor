4848743a0ada801ddd988df22cd6ce7a1cc94f7a
Matt Stein <matt@pixelandtonic.com>
2021-07-22 19:31:53 -0500

    Format.

--- gulpfile.js
+++ gulpfile.js
@@ -5,15 +5,15 @@ const cleanCSS = require('gulp-clean-css');
 const autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('override-css', () => {
-    return gulp.src(`src/assets/field/dist/*.scss`)
-        .pipe(sourcemaps.init())
-        .pipe(sass({
-            outputStyle: 'compact'
-        }).on('error', sass.logError))
-        .pipe(autoprefixer())
-        .pipe(cleanCSS())
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('src/assets/field/dist'));
+  return gulp.src(`src/assets/field/dist/*.scss`)
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      outputStyle: 'compact'
+    }).on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(cleanCSS())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('src/assets/field/dist'));
 });
 
 gulp.task('default', ['override-css']);
--- src/assets/field/dist/init.js
+++ src/assets/field/dist/init.js
@@ -1,59 +1,58 @@
 async function initCkeditor(id, init) {
-    if (typeof CKEDITOR !== 'undefined') {
-        // CKEditor 4
-        const realInit = async function() {
-            const editor = await init();
-            // Keep the source element updated with changes
-            editor.on('change', () => {
-                editor.updateElement();
-            });
-        };
-        const deinit = function() {
-            if (typeof CKEDITOR.instances[id] !== 'undefined') {
-                CKEDITOR.instances[id].updateElement();
-                CKEDITOR.instances[id].destroy();
-                CKEDITOR.remove(id);
-            }
-        };
-        await realInit();
-        // Deinitialize the editor whenever it's being moved
-        // https://ckeditor.com/old/comment/126522#comment-126522
-        Garnish.on(Craft.Preview, 'beforeOpen beforeClose', deinit);
-        Garnish.on(Craft.Preview, 'open close', realInit);
-        Garnish.on(Craft.LivePreview, 'beforeEnter beforeExit', deinit);
-        Garnish.on(Craft.LivePreview, 'enter exit', realInit);
-    } else {
-        // CKEditor 5
-        try {
-            const editor = await init();
-            // Keep the source element updated with changes
-            editor.model.document.on('change', () => {
-                editor.updateSourceElement();
-            });
-        } catch (error) {
-            if (error.message.includes('editor-wrong-element')) {
-                // Try again with a <div>
-                const $textarea = $(`#${id}`);
-                const $input = $('<input/>', {
-                    type: 'hidden',
-                    name: $textarea.attr('name'),
-                    val: $textarea.val(),
-                }).insertAfter($textarea);
-                $textarea.replaceWith(
-                    $('<div/>', {
-                        id: id,
-                        html: $textarea.val(),
-                    })
-                );
-                const editor = await init();
-                // Keep the source element updated with changes
-                editor.model.document.on('change', () => {
-                    $input.val(editor.getData());
-                });
-            } else {
-                throw error;
-            }
-        }
-
+  if (typeof CKEDITOR !== 'undefined') {
+    // CKEditor 4
+    const realInit = async function() {
+      const editor = await init();
+      // Keep the source element updated with changes
+      editor.on('change', () => {
+        editor.updateElement();
+      });
+    };
+    const deinit = function() {
+      if (typeof CKEDITOR.instances[id] !== 'undefined') {
+        CKEDITOR.instances[id].updateElement();
+        CKEDITOR.instances[id].destroy();
+        CKEDITOR.remove(id);
+      }
+    };
+    await realInit();
+    // Deinitialize the editor whenever it's being moved
+    // https://ckeditor.com/old/comment/126522#comment-126522
+    Garnish.on(Craft.Preview, 'beforeOpen beforeClose', deinit);
+    Garnish.on(Craft.Preview, 'open close', realInit);
+    Garnish.on(Craft.LivePreview, 'beforeEnter beforeExit', deinit);
+    Garnish.on(Craft.LivePreview, 'enter exit', realInit);
+  } else {
+    // CKEditor 5
+    try {
+      const editor = await init();
+      // Keep the source element updated with changes
+      editor.model.document.on('change', () => {
+        editor.updateSourceElement();
+      });
+    } catch (error) {
+      if (error.message.includes('editor-wrong-element')) {
+        // Try again with a <div>
+        const $textarea = $(`#${id}`);
+        const $input = $('<input/>', {
+          type: 'hidden',
+          name: $textarea.attr('name'),
+          val: $textarea.val(),
+        }).insertAfter($textarea);
+        $textarea.replaceWith(
+          $('<div/>', {
+            id: id,
+            html: $textarea.val(),
+          })
+        );
+        const editor = await init();
+        // Keep the source element updated with changes
+        editor.model.document.on('change', () => {
+          $input.val(editor.getData());
+        });
+      } else {
+        throw error;
+      }
     }
+  }
 }
