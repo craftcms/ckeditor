5becbecfc6badfabecad253ad88baba6bf23a61b
Tim Kelty <tim@craftcms.com>
2023-04-12 13:22:43 -0400

    Update deps, its builds

--- src/web/assets/ckeditor/webpack.config.js
+++ src/web/assets/ckeditor/webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
 
   // https://webpack.js.org/configuration/output/
   output: {
-    path: path.resolve(__dirname, '../dist'),
+    path: path.resolve(__dirname, 'dist'),
     filename: 'ckeditor.js',
   },
 
