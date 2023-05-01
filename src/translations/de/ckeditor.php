bbb637887621d1756e8bfebfe6fc2ab554cd355f
brandonkelly <brandon@pixelandtonic.com>
2023-05-01 10:36:02 -0700

    Pull in Redactor t9n messages

---
+++ src/translations/de/ckeditor.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'Link to an entry' => 'Link zu einem Eintrag',
+    'Link to an asset' => 'Link zu einer Datei',
+    'Link to a category' => 'Link zu einer Kategorie',
+];
---
+++ src/translations/nl/ckeditor.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'Link to a category' => 'Link naar een categorie',
+    'Link to an entry' => 'Link naar inhoud',
+];
