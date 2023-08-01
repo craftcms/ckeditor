edaea84d33ca2f5804f117c8cfa0fb828212a68f
Iwona Just <iwona@pixelandtonic.com>
2023-08-01 12:00:57 +0100

    add no translation file (copy of nb)

---
+++ src/translations/no/ckeditor.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'Advanced' => 'Avansert',
+    'Available Transforms' => 'Tilgjengelige bildeomforminger',
+    'Changing this may result in data loss.' => 'Å endre denne kan føre til tap av data',
+    'Column Type' => 'Kolonnetype',
+    'Disable this at your own risk!' => 'Deaktiver på eget ansvar!',
+    'HTML Purifier Config' => 'Instillinger for HTML-rens',
+    'Link to a category' => 'Link til en kategori',
+    'Link to an entry' => 'Link til en artikkel',
+    'Purify HTML' => 'Rens HTML-koden',
+    'The type of column this field should get in the database.' => 'Kolonnetypen dette feltet skal ha i databasen.',
+    'View available settings' => 'Vis tilgjengelig instillinger',
+    'You can save custom {name} configs as {ext} files in {path}.' => 'Du kan lagre egendefinerte {name} instillinger som {ext} filer i {path}.',
+];
