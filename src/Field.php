49dd8af1965fe16d9f7e8f744bc166286f7859e5
brandonkelly <brandon@pixelandtonic.com>
2023-08-10 22:37:14 -0700

    ECS

--- src/Field.php
+++ src/Field.php
@@ -171,7 +171,7 @@ class Field extends HtmlField
                             ]),
                         );
                     }
-                }
+                },
             ];
         }
 
