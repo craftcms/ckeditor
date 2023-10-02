560666c6ab29329d4ed1a7d4b36aad60414eb058
brandonkelly <brandon@pixelandtonic.com>
2023-10-02 10:07:58 +0200

    Call prepForEagerLoading()

--- src/Field.php
+++ src/Field.php
@@ -910,10 +910,8 @@ JS,
                 },
             ], true));
 
-            // Set the query up for lazy eager loading
-            $query->eagerLoadSourceElement = $owner;
-            $providerHandle = $owner->getFieldLayout()?->provider?->getHandle();
-            $query->eagerLoadHandle = $providerHandle ? "$providerHandle:$this->handle" : $this->handle;
+            // Prepare the query for lazy eager loading
+            $query->prepForEagerLoading($this->handle, $element);
         } else {
             $query->id = false;
         }
