9bb8124426c07cb1fa0826af51dd5ce97a0c4e76
i-just <iwona@pixelandtonic.com>
2025-08-04 15:41:13 +0100

    remove invisible chars

--- src/Field.php
+++ src/Field.php
@@ -818,6 +818,8 @@ class Field extends HtmlField implements ElementContainerFieldInterface, Mergeab
             return null;
         }
 
+        $value = preg_replace(StringHelper::invisibleCharsRegex(), '', $value);
+
         // Redactor to CKEditor syntax for <figure>
         // (https://github.com/craftcms/ckeditor/issues/96)
         $value = $this->_normalizeFigures($value);
